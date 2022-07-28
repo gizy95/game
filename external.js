404776f158ef98b5356f5559149366dec0b837f4
antonio <bjelobrka@gmail.com>
2022-07-28 19:33:42 +0200

    ASdded playerSelection function which lets the user write what move he wants to use and then returns it. The input is not case sensitive so the user can enter the word how he wants.

--- external.js
+++ external.js
@@ -3,17 +3,44 @@ function getComputerChoice(){
     let move;
 
     if(randomNumber == 1){
-        move = "rock";
+        move = "Rock";
 
     }else if(randomNumber == 2){
-        move = "papper";
+        move = "Papper";
 
     }else if(randomNumber == 3){
-        move = "scissor";
+        move = "Scissor";
 
     }
 
     return move;
 }
 
+
+function playerSelection(){
+
+    let playerInput = prompt("Enter Rock,Paper or Scissor to defeat the machine");
+    let capLetter = playerInput.substring(0,1);
+    let rest = playerInput.substring(1);
+    playerInput = capLetter.toUpperCase() + rest.toLowerCase();
+    let move;
+
+    if(playerInput.trim() == "Rock"){
+        move = "Rock";
+    } else if(playerInput.trim() == "Papper"){
+        move = "Papper";
+    } else if(playerInput.trim() == "Scissor"){
+        move = "Scissor";
+    }else{
+        alert("Input was not valid try again");
+    }
+
+    return move;
+    
+
+    
+
+}
+
 console.log(getComputerChoice());
+console.log(playerSelection());

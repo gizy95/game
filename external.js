34e68312c60ebe86f367c24e4da04675a19ae474
antonio <bjelobrka@gmail.com>
2022-07-28 19:01:00 +0200

    Added the getComputerChoice function which generates a random number from 1 to 3 and then chooses his move depending on the number

--- external.js
+++ external.js
@@ -1,1 +1,19 @@
-console.log("Hello");
+function getComputerChoice(){
+    let randomNumber = Math.floor(Math.random()*3) + 1;
+    let move;
+
+    if(randomNumber == 1){
+        move = "rock";
+
+    }else if(randomNumber == 2){
+        move = "papper";
+
+    }else if(randomNumber == 3){
+        move = "scissor";
+
+    }
+
+    return move;
+}
+
+console.log(getComputerChoice());
